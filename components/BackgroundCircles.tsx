b5bc3078c31d5e0494fdd245b4e3956bcfb95c9e
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-11-19 17:17:30 +0300

    .

--- components/BackgroundCircles.tsx
+++ components/BackgroundCircles.tsx
@@ -21,9 +21,9 @@ const BackgroundCircles = (props: Props) => {
     >
       <div className="absolute border [#33333] rounded-full h-[200px] w-[200px] mt-52  opacity-20 animate-ping" />
       <div className="absolute border [#33333] rounded-full h-[300px] w-[300px] mt-52  opacity-20" />
-      <div className="absolute border [#33333] rounded-full h-[500px] w-[500px] mt-52 " />
+      <div className="absolute border [#33333] rounded-full h-[500px] w-[500px] mt-52  opacity-20 " />
       <div className="absolute border rounded-full border[#F7AB0A] opacity-20 mt-52 h-[650px] w-[650px] animate-pulse" />
-      <div className="absolute border rounded-full border[#33333] mt-52 h-[800px] w-[800px] " />
+      <div className="absolute border rounded-full border[#33333] mt-52 h-[800px] w-[800px]   opacity-20 " />
     </motion.div>
   );
 };
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { Typewriter, useTypewriter, Cursor } from "react-simple-typewriter";
 import BackgroundCircles from "./BackgroundCircles";
+// import Image from "next/image";
 type Props = {};
+import ppImg from "../assets/pp.jpg";
+
+const ppImage = ppImg;
 
 const Hero = (props: Props) => {
   const [text, count] = useTypewriter({
@@ -16,8 +20,18 @@ const Hero = (props: Props) => {
   return (
     <div className=" h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
       <BackgroundCircles />
-      <h1>
-        <span> {text} </span>
+      {/* change this to nextJS images later on */}
+      {/* these are the classes to add your Image */}
+      {/* <img src="" alt="" className="relative rounded-full h-32 w-32 mx-auto" /> */}
+      <div
+        style={{ border: "2px solid white" }}
+        className=" relative rounded-full h-32 w-32 mx-auto"
+      />
+      <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+        Blockchain developer
+      </h2>
+      <h1 className="text-5xl lg:text-6xl font-semibold scroll-px-10">
+        <span className="mr-3"> {text} </span>
         <Cursor cursorColor="#F7AB0A" />
       </h1>
     </div>
