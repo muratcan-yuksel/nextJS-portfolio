b8e97d1a77ce89c04c11575f22443d21a7c34b8e
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-11-22 22:23:31 +0300

    add image

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,6 +4,8 @@ import BackgroundCircles from "./BackgroundCircles";
 // import Image from "next/image";
 type Props = {};
 import ppImg from "../assets/pp.jpg";
+import Link from "next/link";
+import Image from "next/image";
 
 const ppImage = ppImg;
 
@@ -20,20 +22,37 @@ const Hero = (props: Props) => {
   return (
     <div className=" h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
       <BackgroundCircles />
-      {/* change this to nextJS images later on */}
-      {/* these are the classes to add your Image */}
-      {/* <img src="" alt="" className="relative rounded-full h-32 w-32 mx-auto" /> */}
-      <div
-        style={{ border: "2px solid white" }}
-        className=" relative rounded-full h-32 w-32 mx-auto"
+      <Image
+        src={ppImage}
+        alt=""
+        className="relative rounded-full h-32 w-32 mx-auto"
       />
-      <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
-        Blockchain developer
-      </h2>
-      <h1 className="text-5xl lg:text-6xl font-semibold scroll-px-10">
-        <span className="mr-3"> {text} </span>
-        <Cursor cursorColor="#F7AB0A" />
-      </h1>
+      <div className="z-20">
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+          Blockchain developer
+        </h2>
+        <div className="">
+          <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+            <span className="mr-3"> {text} </span>
+            <Cursor cursorColor="#F7AB0A" />
+          </h1>
+
+          <div className="pt-5">
+            <Link href="#about">
+              <button className="heroButton">About </button>
+            </Link>
+            <Link href="#experience">
+              <button className="heroButton">Experience </button>
+            </Link>
+            <Link href="#skills">
+              <button className="heroButton">Skills </button>
+            </Link>
+            <Link href="#projects">
+              <button className="heroButton">Projects </button>
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
