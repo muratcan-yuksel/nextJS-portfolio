9e3a82cf6311dd84f75ad0eb65ed51d9f50a25be
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-11-24 17:06:04 +0300

    working on contact

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -1,5 +1,11 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from "react";
+import {
+  PhoneIcon,
+  MappinIcon,
+  EnvelopeIcon,
+  MapPinIcon,
+} from "@heroicons/react/24/solid";
 
 type Props = {};
 
@@ -14,6 +20,42 @@ const ContactMe = (props: Props) => {
           I've got just what you need. <br />
           <span className="decoration-[#F7AB0A]/50 underline">Let's Talk.</span>
         </h4>
+        <div className="space-y-10">
+          <div className="flex items-center space-x-5 justify-center">
+            <PhoneIcon className="text-[#F7Ab0A] h-7 w-7 animate-pulse" />
+            <p className="text-2xl">+905526931892</p>
+          </div>
+
+          <div className="flex items-center space-x-5 justify-center">
+            <EnvelopeIcon className="text-[#F7Ab0A] h-7 w-7 animate-pulse" />
+            <p className="text-2xl">muratcanyukselpro@gmail.com</p>
+          </div>
+
+          <div className="flex items-center space-x-5 justify-center">
+            <MapPinIcon className="text-[#F7Ab0A] h-7 w-7 animate-pulse" />
+            <p className="text-2xl">23 dev lane</p>
+          </div>
+        </div>
+        <form className="flex flex-col space-y-2 w-fit mx-auto">
+          <div className="flex space-x-2">
+            <input placeholder="Name" className="contactInput" type="text" />
+            <input placeholder="Email" className="contactInput" type="text" />
+          </div>
+          <input placeholder="Subject" className="contactInput" type="text" />
+
+          <textarea
+            placeholder="Message"
+            className="contactInput"
+            name=""
+            id=""
+          />
+          <button
+            type="submit"
+            className="bg-[#F7Ab0A] py-5 px-10 rounded-md text-black font-bold"
+          >
+            Submit
+          </button>
+        </form>
       </div>
     </div>
   );
