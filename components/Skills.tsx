34a55aed91efd91e1ce0c70344f26a0ea07f4202
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-11-24 13:39:55 +0300

    skills done, but need to fix the animation part

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -6,7 +6,12 @@ type Props = {};
 
 const Skills = (props: Props) => {
   return (
-    <motion.div className="h-screen flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen xl:space-y-0 mx-auto items-center">
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className=" flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center"
+    >
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         Skills
       </h3>
