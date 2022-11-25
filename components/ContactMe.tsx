03caa78787f7a2499c06bbae3ccf688b2463f1ff
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-11-26 01:56:59 +0300

    add forms and fix hydration issue

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -1,15 +1,20 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from "react";
-import {
-  PhoneIcon,
-  MappinIcon,
-  EnvelopeIcon,
-  MapPinIcon,
-} from "@heroicons/react/24/solid";
+import { PhoneIcon, EnvelopeIcon, MapPinIcon } from "@heroicons/react/24/solid";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
+};
 
 type Props = {};
 
-const ContactMe = (props: Props) => {
+const ContactMe = ({}: Props) => {
+  const { register, handleSubmit } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (formData) => console.log(formData);
   return (
     <div className="h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
@@ -36,14 +41,33 @@ const ContactMe = (props: Props) => {
             <p className="text-2xl">23 dev lane</p>
           </div>
         </div>
-        <form className="flex flex-col space-y-2 w-fit mx-auto">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col space-y-2 w-fit mx-auto"
+        >
           <div className="flex space-x-2">
-            <input placeholder="Name" className="contactInput" type="text" />
-            <input placeholder="Email" className="contactInput" type="text" />
+            <input
+              {...register("name", { required: true })}
+              placeholder="Name"
+              className="contactInput"
+              type="text"
+            />
+            <input
+              {...register("email", { required: true })}
+              placeholder="Email"
+              className="contactInput"
+              type="email"
+            />
           </div>
-          <input placeholder="Subject" className="contactInput" type="text" />
+          <input
+            {...register("subject", { required: true })}
+            placeholder="Subject"
+            className="contactInput"
+            type="text"
+          />
 
           <textarea
+            {...register("message", { required: true })}
             placeholder="Message"
             className="contactInput"
             name=""
