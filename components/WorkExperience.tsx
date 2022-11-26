4e5f5d428d7fae8f0abe13709df4f259b69ac81a
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-11-26 05:27:01 +0300

    fallback

--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -12,10 +12,10 @@ const WorkExperience = (props: Props) => {
       transition={{ duration: 1.5 }}
       className="h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center"
     >
-      <h3 className="absolute top-10 upercase tracking-[20px] text-gray-500 text-2xl">
+      <h3 className="md:absolute top-0 mb-10 upercase tracking-[20px] text-gray-500 text-2xl">
         Experience
       </h3>
-      <div className="w-full  top-36 flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
+      <div className="w-full md:mt-36 xl:mt-10 top-36 flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
         <ExperienceCard />
         <ExperienceCard />
         <ExperienceCard />
